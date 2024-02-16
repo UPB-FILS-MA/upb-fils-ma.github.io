481644dcdcd5089b31a0851f981737be128f2a40
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2024-02-16 15:16:19 +0200

    Add welcome page (#12)
    * add the welcome page, with the team
    
    * define the takeaways of the lectures
    
    * usa a table for the team
    
    * delete IoT group
    
    * update timetable to show the activities
    
    * fix timetable typo
    
    * fix typo
    
    Co-authored-by: Irina Nita <74008735+irina-nita@users.noreply.github.com>
    
    * use Danut's university email
    
    Co-authored-by: George Danut Aldea <45511762+DanutAldea@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Irina Nita <74008735+irina-nita@users.noreply.github.com>
    Co-authored-by: George Danut Aldea <45511762+DanutAldea@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/lecture">
+            to="/docs/welcome">
             Get started
           </Link>
         </div>
