1f1884c7c4582460f723d22f1d131c68a3de9283
Irina Bradu <72762129+irina-b-dev@users.noreply.github.com>
2024-03-20 11:29:57 +0200

    Lab 03 - Interrupts and timers (#63)
    * added lab03
    
    * fix phrasing
    
    * lab03 guidelines
    
    * added interrupts + objdump , changed timers and exercises
    
    * add small fix
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update docs/lab/03/03.md
    
    Co-authored-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    
    * Update 03.md
    
    * reading ok
    
    * Update docs/lab/03/03.md
    
    ---------
    
    Co-authored-by: Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,6 +69,12 @@ const config: Config = {
     },
   ],
 
+  markdown: {
+    mermaid: true,
+  },
+
+  themes: ['@docusaurus/theme-mermaid'],
+
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.png',
