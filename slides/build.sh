0abe9df77410a7a7482430bb95eaf95949def946
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2024-04-06 22:56:51 +0300

    improved building

--- slides/build.sh
+++ slides/build.sh
@@ -4,7 +4,7 @@ for slides in $(basename -a $(find lectures/* -maxdepth 0 -type d)); do
     if [ $slides != "assets" ]; then
         echo "Building slides $slides"
         ln -s ../assets/global-bottom.vue lectures/$slides/global-bottom.vue
-        npm run build -- lectures/$slides/slides.md --base /slides/$slides --out ../../../build/slides/$slides
+        npm run build -- lectures/$slides/slides.md --base /slides/$slides --out ../../../static/slides/$slides
         fail=$?
         rm lectures/$slides/global-bottom.vue
         if [ $fail != 0 ]; then
--- slides/lectures/01/global-bottom.vue
+++
@@ -1,1 +0,0 @@
-../assets/global-bottom.vue
